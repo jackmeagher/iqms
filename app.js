ff591229a630f6a4f8d767066e54d347fd97d675
Jack Meagher <meagherje@g.cofc.edu>
2016-01-22 16:50:15 -0500

    Establish error handling paradigm
    Figured out a way to handle errors in node without creating the
    so-called "pyramid of death" that looks like this:
    
    function(err, cb) {
        if (!err) {
            cb(function(err, cb) {
                if (!err) {
                    cb(function(err, cb)) {
                        //stuff
                    }
                } else {
                    // Handle error
                }
            }
        } else {
            // Handle error
        }
    }
    
    instead we can do this:
    
    function (err, cb) {
        if (err) {
            // Handle error
            return;
        }
        cb( function(err, cb) {
            if (err) {
                // Handle error
                return;
            }
        }
    }

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ require("use-strict");
 var express = require('express');
 var app = express();
 
+var fun = function(argument, callback) {
+    var err = new Error('ERROR!');
+    err = null;
+    callback(err, argument);
+}
+
 app.get('/', (req, res) => {
-    res.send("Hello, world!");
+    fun("Hello, world!", (err, arg) => {
+        if (err) {
+            console.error("Error occurred!", err);
+            res.json({success: false, error: err});
+            return;
+        }
+        res.json({success: true, msg: "Hello, world!"});
+    });
 });
 
 app.listen(3000, () => {
