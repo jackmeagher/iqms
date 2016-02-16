7f0bd9a5cb3b8a1d68c76e9f85f5198de8fb4850
Jack Meagher <meagherje@g.cofc.edu>
2016-02-16 17:10:04 -0500

    Remove whitespace

--- app.js
+++ app.js
@@ -5,15 +5,8 @@ var Resource = require('./lib/Resource');
 var models = require('./models');
 var routes = require('./routes');
 
-
-
-
-
-
 routes.hw_resource.register(app, '');
 routes.question.register(app,'');
 routes.interview.register(app,'');
-
-
 
 module.exports = app;
