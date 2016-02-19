ba74a62824a33aa4ee627cb370b1f59011b37e62
desistonv <desistonv@g.cofc.edu>
2016-02-18 22:01:53 -0500

    cross origin req headers

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ var routes = require('./routes');
 
 
 
+// middleware to add headers for cross origin requests
 
+app.all('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
+    next();
+});
 
 routes.hw_resource.register(app, '');
 routes.question.register(app,'');
 routes.interview.register(app,'');
+
 
 
 
