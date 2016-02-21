230bfa22ad85086e4d89549a1d3ddcd0bea1cd62
Jack Meagher <meagherje@g.cofc.edu>
2016-02-21 13:10:49 -0500

    Remove an accidentally left-in merge conflict tag

--- test/test.js
+++ test/test.js
@@ -28,7 +28,6 @@ describe('App', function() {
                             },
                             done
                         );
-<<<<<<< Updated upstream
                 });
             });
         });
