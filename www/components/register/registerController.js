50b4be24cd180d4fe6d3eebf5e7f858f89bc9cbe
shockleyje <shockleyje@g.cofc.edu>
2016-03-03 15:39:43 -0500

    added components and services for register

---
+++ www/components/register/registerController.js
@@ -0,0 +1,32 @@
+/**
+ * Created by James on 3/2/2016.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('registerController', registerController);
+
+    registerController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
+    function registerController(UserService, $location, $rootScope, FlashService) {
+        var vm = this;
+
+        vm.register = register;
+
+        function register() {
+            vm.dataLoading = true;
+            UserService.create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful', true);
+                        $location.path('/index');
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+    }
+
+})();
