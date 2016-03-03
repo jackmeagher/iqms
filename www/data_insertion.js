aeca649fbf81df3fe0c31d638ca16d6ee2de1570
Malcolm Byrd <malcolmbyrd@users.noreply.github.com>
2016-03-03 13:41:37 -0500

    bug fixes

--- www/data_insertion.js
+++ www/data_insertion.js
@@ -3,22 +3,20 @@
  */
 
 function data_insertion($scope, $http) {
-    $http.get('http://127.0.0.1:3000/question').
-    success(function(data) {
+    $http.get('http://127.0.0.1:3000/question').success(function (data) {
         $scope.questions = data;
     });
 
-    $http.get('http://127.0.0.1:3000/interview').
-    success(function(data) {
+    $http.get('http://127.0.0.1:3000/interview').success(function (data) {
         $scope.interview = data;
     });
 
-    $http.get('http://127.0.0.1:3000/answer').
-    success(function(data) {
+    $http.get('http://127.0.0.1:3000/answer').success(function (data) {
         $scope.answers = data;
     });
-    $http.get('http://127.0.0.1:3000/user').
-    success(function(data) {
-        $scope.users = data;
 
-})};
+    $http.get('http://127.0.0.1:3000/user').success(function (data) {
+        $scope.users = data;
+    });
+};
+
