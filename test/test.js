fee96f7bfe3848080b8f31c03c6ad10561aea5ba
shockleyje <shockleyje@g.cofc.edu>
2016-03-22 15:51:09 -0400

    added interview tests

--- test/test.js
+++ test/test.js
@@ -47,7 +47,55 @@ describe('App', function() {
             });
         });
     });
+
+    describe('/interview', function() {
+        var url = '/interview';
+        describe('#GET', function() {
+
+            it('should return response code 200', function(done) {
+                server_promise.then( (server) => {
+                    request(server)
+                        .get(url)
+                        .expect(200, done);
+                });
+            });
+            it('should return success true and no data', function(done) {
+                server_promise.then( (server) => {
+                    request(server)
+                        .get(url)
+                        .expect(200, {
+                                success: true,
+                                interviews: []
+                            },
+                            done
+                        );
+                });
+            });
+        });
+
+        describe('#POST', function() {
+            it('should insert a question', function(done) {
+                server_promise.then( (server) => {
+                    var payload = {
+                        /*
+                        not sure what should go here
+                        since routes/interview.js
+                        create({}) is not
+                        fleshed out with data values
+                        */
+                    };
+                    request(server)
+                        .post(url)
+                        .send(payload)
+                        .expect(200, payload, done);
+                });
+            });
+        });
+    });
 });
+
+
+
 
 /*describe('/question', function() {
     describe('#POST', function() {
