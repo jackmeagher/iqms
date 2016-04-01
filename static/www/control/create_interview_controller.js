f783e7804da9ceb7555c2fb2e3e02c8bde3d4741
nick <desistonv@g.cofc.edu>
2016-03-31 21:08:38 -0400

    create interview page now functional

--- static/www/control/create_interview_controller.js
+++ static/www/control/create_interview_controller.js
@@ -5,16 +5,18 @@
  * Created by Nicholas Desisto on 3/31/16.
  */
 
-function create_interview_controller($scope, $http) {
+function create_interview_controller($scope, $http,$window) {
 
     $scope.CreateInterview = function () {
         inter = document.getElementById("interviewee").value;
         job_title = document.getElementById("job_title").value;
         console.log(job_title);
-        var par1 = JSON.stringify({interviewee: inter, label: job_title});
-        var par1 = JSON.stringify({hello :'hello'});
+        var par1 = {interviewee: inter, label: job_title};
+        //var par1 = JSON.stringify({hello :'hello'});
 
         $http.post('/interview',par1);
+        $window.location.href = './listInterviews.html';
+
     }
 };
 
