2e5901b9717ac241a6dfd8d3d95a663eef8a35e1
Jack Meagher <meagherje@g.cofc.edu>
2016-04-05 10:51:42 -0400

    Fix users tests

---
+++ test/questions.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jack on 4/5/16.
+ */
--- test/users.js
+++ test/users.js
@@ -4,81 +4,116 @@ var request = require('supertest');
 var server_promise = require('../bin/www_test');
 
 var expectedUsersData = {
-'username' : 'testman username',
-'first_name' : 'testman first',
-'last_name' : 'testman last'
+    'username': 'testman_username',
+    'first_name': 'testman_first',
+    'last_name': 'testman_last',
+    'email': 'testman@gmail.com'
 };
 
-describe('App', function() {
-    before( function() {
+describe('App', function () {
+    before(function () {
         server_promise = require('../bin/www_test');
     });
-    describe('/user', function() {
+    describe('/user', function () {
         var url = '/user';
-        describe('#GET /user ', function() {
+        describe('#GET /user ', function () {
 
-            it('should return content as type json', function(done) {
-                server_promise.then( (server) => {
+            it('should return content as type json', function (done) {
+                server_promise.then((server) => {
                     request(server)
                         .get(url)
+                        .set('Accept', 'application/json')
                         .expect('Content-Type', /json/)
-                        .expect(200, done)
-                        .end(function (err, res) {
-                          if (err) return done(err);
-                        });
-                        done();
+                        .expect(200, done);
                 });
             });
-            it('should have a property (id)', function(done) {
-                server_promise.then( (server) => {
+            it('should have a property users', function (done) {
+                server_promise.then((server) => {
                     request(server)
                         .get(url)
-                        .expect(200, done)
+                        .expect(function(res) {
+                            if (!res.body || !res.body.users) {
+                                throw new Error("No users field returned.");
+                            }
+                        })
                         .end(function (err, res) {
-                          res.body[0].should.have.property('id');
-                          done();
+                            if (err) {
+                                done(err);
+                            } else {
+                                done();
+                            }
                         });
-                        done();
                 });
             });
-            it('should return expected users', function(done) {
-                server_promise.then( (server) => {
-                    request(server)
-                        .get(url)
-                        .expect(200)
-                        .end(function (err, res) {
-                          //TODO: Define test data and adjust
-                          var expectedUsers = ['add', 'test', 'data'];
 
-
-                          error(err, 'No error');
-                          same(res.body, expectedUsers, 'Users as expected');
-                          end();
-                        });
-                        done();
-                });
-            });
         });
 
-        describe('#POST', function() {
-            it('should add a user', function(done) {
-                server_promise.then( (server) => {
+        describe('#POST', function () {
+            it('should add a user', function (done) {
+                server_promise.then((server) => {
                     var payload = expectedUsersData;
 
                     request(server)
                         .post(url)
                         .send(payload)
-                        .expect('Content-Type', /json/)
-                        .expect(function(res) {
-                          if (!res.body == payload) throw new error("Didn't get expected data back")
+                        .expect(function (res) {
+                            if (!res.body.data.username == payload.username) {
+                                throw new Error("Didn't get expected username back.");
+                            }
+                            if (!res.body.data.first_name == payload.first_name) {
+                                throw new Error("Didn't get expected last name back.")
+                            }
+                            if (!res.body.data.last_name == payload.last_name) {
+                                throw new Error("Didn't get expected last name")
+                            }
                         })
                         .end(function (err, res) {
-                            if (err) return done(err);
-                          done();
+                            if (err) {
+                                done(err);
+                            } else {
+                                done();
+                            }
                         });
                 });
             });
         });
+
+        describe('#GET2', function () {
+            it('should return the user we added earlier', function (done) {
+                server_promise.then((server) => {
+                    request(server)
+                        .get(url)
+                        .expect(function (res) {
+                            if (!res.body || !res.body.users) {
+                                throw new Error("No users field returned.");
+                            }
+                            if (!res.body.users[0]) {
+                                throw new Error("No users returned, even though one was just added.");
+                            }
+                            if (!res.body.users[0].email == expectedUsersData.email) {
+                                throw new Error("Wrong email returned.");
+                            }
+                            if (!res.body.users[0].username == expectedUsersData.username) {
+                                throw new Error("Wrong username returned.");
+                            }
+                            if (!res.body.users[0].first_name == expectedUsersData.first_name) {
+                                throw new Error("Wrong first name returned.");
+                            }
+                            if (!res.body.users[0].last_name == expectedUsersData.last_name) {
+                                throw new Error("Wrong last name returned.");
+                            }
+                        }).end(function (err, res) {
+                        if (err) {
+                            done(err);
+                        } else {
+                            done();
+                        }
+
+                    });
+                });
+            });
+
+        })
 
 
     });
