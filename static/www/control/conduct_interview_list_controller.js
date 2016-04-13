f80879c0cf153a6f538a335daaeffadbc95ac39c
desistonv <desistonv@g.cofc.edu>
2016-04-12 22:33:04 -0400

    conduct interview page now properly takes id as route param and the controller gets the interview and its questions

--- static/www/control/conduct_interview_list_controller.js
+++ static/www/control/conduct_interview_list_controller.js
@@ -1,3 +1,29 @@
 /**
  * Created by nick on 4/12/16.
  */
+/**
+ * Created by nick on 3/31/16.
+ */
+
+function conduct_interview_list_controller($scope, $http) {
+
+    $scope.sortType     = 'interviewee'; // set the default sort type
+    $scope.sortReverse  = false;  // set the default sort order
+    $scope.searchInterview  = '';     // set the default search/filter term
+
+
+    $http.get('/interview').success(function (data) {
+        $scope._interview = data;
+    });
+
+
+    $scope.DeleteInterview = function (index,interview) {
+        $http.delete('/interview/' + interview.id)
+            .success( function(){
+                $http.get('/interview').success(function(data){
+                    $scope._interview = data;
+                })}
+            )};
+
+};
+
--- static/www/control/list_interview_controller.js
+++ static/www/control/list_interview_controller.js
@@ -4,7 +4,7 @@
 
 function list_interview_controller($scope, $http) {
 
-    $scope.sortType     = 'question_text'; // set the default sort type
+    $scope.sortType     = 'interviewee'; // set the default sort type
     $scope.sortReverse  = false;  // set the default sort order
     $scope.searchInterview  = '';     // set the default search/filter term
 
