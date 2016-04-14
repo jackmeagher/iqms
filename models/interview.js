cbd15858ea1ed3dc8f8f1aece66d7cbbc0e8487f
Jack Meagher <meagherje@g.cofc.edu>
2016-04-14 16:24:29 -0400

    Fix typo

--- models/interview.js
+++ models/interview.js
@@ -3,7 +3,7 @@
 module.exports = function(sequelize, DataTypes) {
     var interview = sequelize.define("interview", {
             label: DataTypes.STRING,
-            interview: DataTypes.STRING,
+            interviewee: DataTypes.STRING
         },
 
         {
