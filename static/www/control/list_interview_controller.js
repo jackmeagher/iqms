f0863b8e59bb7d0505e1e3a2cd71bba2cd97b700
shockleyje <shockleyje@g.cofc.edu>
2016-04-14 12:51:56 -0400

    commented out dropdown code because apparently I can't frontend

--- static/www/control/list_interview_controller.js
+++ static/www/control/list_interview_controller.js
@@ -12,22 +12,23 @@ function list_interview_controller($scope, $http) {
     $http.get('/interview').success(function (data) {
         $scope._interview = data;
         //get all question_text associated with interviews
-        $scope._interviews.forEach(q => $http.get('/interview/' + q.id + '/questions').success(function(data){
-            the_questions = data.questions;
-            q.questions = '';
-            console.log(the_questions.length);
-            if (the_questions.length > 1){
-                for(var i = 0;i < the_questions.length-1;i++){
-                    q.questions += the_questions[0].question_text;
-                    q.questions += ','
-                }
-                q.questions += the_questions[i].question_text;
-
-            }else if(the_questions.length == 1){
-                q.questions = the_questions[0].questions_text;
-            }
-
-        }));
+        //TODO: fix
+        // $scope._interviews.forEach(q => $http.get('/interview/' + q.id + '/questions').success(function(data){
+        //     the_questions = data.questions;
+        //     q.questions = '';
+        //     console.log(the_questions.length);
+        //     if (the_questions.length > 1){
+        //         for(var i = 0;i < the_questions.length-1;i++){
+        //             q.questions += the_questions[0].question_text;
+        //             q.questions += ','
+        //         }
+        //         q.questions += the_questions[i].question_text;
+        //
+        //     }else if(the_questions.length == 1){
+        //         q.questions = the_questions[0].questions_text;
+        //     }
+        //
+        // }));
         //end get all question_text associated with the interviews
     });
 
