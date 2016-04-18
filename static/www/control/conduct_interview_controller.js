cb1d2328aa6046295e4f43c8a12216e2f2afdeff
desistonv <desistonv@g.cofc.edu>
2016-04-18 06:47:44 -0400

    these stars....

--- static/www/control/conduct_interview_controller.js
+++ static/www/control/conduct_interview_controller.js
@@ -2,33 +2,50 @@
  * Created by nick on 4/12/16.
  */
 
-function conduct_interview_controller ($scope,$location,$http,$window,$routeParams) {
+
+function conduct_interview_controller ($scope,$location,$http,$window,$routeParams,$interval) {
     var interviewId = $routeParams.id;
     $scope.questions = [];
     $scope.currentQuestion = {};
     $scope.currentAnswer  = '';
     $scope.currentQuestionIndex = 0;
     self.n_questions = 0;
+    currentRating = 0;
 
     function mod(n, m) {
         return ((n % m) + m) % m;
     }
-
-
+    Number.prototype.mod = function(n) {
+        return ((this%n)+n)%n;
+    };
 
     $scope.updateCurrentQuestion = function(i){
-        $scope.currentQuestionIndex = mod(($scope.currentQuestionIndex + i) ,self.n_questions);
-        console.log($scope.currentQuestionIndex);
+
+        $http.put('/answer',{answer:$scope.currentQuestion.answer}).success(function() {
+            r = $scope.currentQuestion.answer.rating;
+                angular.element('input:radio[name="rating"]').filter('[value="' + r + '"]').attr('checked', false);
+
+            $scope.currentQuestionIndex = ($scope.currentQuestionIndex + i).mod(self.n_questions);
 
         $scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
 
 
             $http.get('/answer/' +  interviewId + '/' + $scope.currentQuestion.id).success(function(answer){
-                    $scope.currentQuestion.answer = answer.answer;
-                    document.getElementById('response').value = $scope.currentQuestion.answer.feedback;
 
-            }
-            )
+                $scope.currentQuestion.answer = answer.answer;
+                    console.log('loaded from db ' + answer.answer.rating);
+                    document.getElementById('response').value = $scope.currentQuestion.answer.feedback;
+                    rating = $scope.currentQuestion.answer.rating ? $scope.currentQuestion.answer.rating : 0;
+                //angular.element('#fields_for_stars').val('' + rating);
+                angular.element('input:radio[name="rating"]').filter('[value="' + rating + '"]').prop('checked', true);
+
+                })
+
+
+
+
+
+        });
 
 
 
@@ -47,6 +64,33 @@ function conduct_interview_controller ($scope,$location,$http,$window,$routePara
 
         $scope.click_answer_update();
     });
+
+
+
+    $scope.star_update = function() {
+        var dif = document.getElementById("the_stars");
+        var curr = dif.firstChild;
+        while (curr != null) {
+            if (curr.checked) {
+                break;
+            }
+            curr = curr.nextSibling;
+
+        }
+        if(curr) {
+
+            $scope.currentQuestion.answer.rating = curr.value;
+
+            $http.put('/answer', {answer: $scope.currentQuestion.answer}).success(function () {
+                console.log('updated stars with ' + curr.value);
+            });
+        }
+    };
+
+
+    //$interval($scope.star_update,250);
+
+
 
 
     $http.get('/interview/' + interviewId).success(function (data) {
@@ -87,6 +131,9 @@ function conduct_interview_controller ($scope,$location,$http,$window,$routePara
             $http.get('/answer/' +  interviewId + '/' + $scope.currentQuestion.id).success(function(answer){
                     $scope.currentQuestion.answer = answer.answer;
                 document.getElementById('response').value = $scope.currentQuestion.answer.feedback;
+
+                rating = $scope.currentQuestion.answer.rating ? $scope.currentQuestion.answer.rating : 5;
+                angular.element('input:radio[name="rating"]').filter('[value="' + rating + '"]').prop('checked', true);
 
                 });
 
