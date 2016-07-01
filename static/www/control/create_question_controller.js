6750c565b83173de2320debb069ee57a12732a3f
Hunter Heidenreich <hunter.scott.heidenreich@gmail.com>
2016-07-01 12:25:33 -0400

    Revise create_question_controller for new tagging system

--- static/www/control/create_question_controller.js
+++ static/www/control/create_question_controller.js
@@ -3,28 +3,76 @@
  */
 
 function create_question_controller ($scope,$location,$http,$window, taggingService, popupService) {
-    $scope.qt = $location.search().qt;
-   
-    $scope.questionText = '';
-   
-    $scope.current_topics = [];
-    $scope.current_subtopics = [];
-
-    $scope.types = taggingService.getTypes();
-
-    $scope.answers = [1];
     
-    $scope.newSub = '';
+    $scope.questionText = '';
+    $scope.types = taggingService.getTypes();
+    $scope.currentTags = [];
+    $scope.answers = [''];
     
     $scope.questionData = {
       text: '',
       type: '',
-      topic: '',
-      subtopics: [],
+      tags: [],
       difficulty: 0,
       answers: []
     };
     
+    //Type
+    
+    $scope.updateSelectedType = function(value) {
+        taggingService.updateSelectedType(value);
+        $("#subtopic-box").css({"visibility": "hidden"});
+        $scope.currentTags = taggingService.getCurrentTags();
+    }
+
+    //Tag
+    
+    $scope.$on("topicNotification", function(event, args) {
+        $scope.currentTags = taggingService.getCurrentTags();
+    });
+    
+    //Answer
+    
+    $scope.addAnswer = function() {
+        $scope.answers.push($scope.answers.length + 1);
+    }
+    
+    $scope.removeAnswer = function() {
+        if ($scope.answers.length > 1) {
+            $scope.answers.pop();
+        }
+    }
+    
+    //Submit
+    
+    $scope.compileData = function () {
+    
+        $scope.questionData.text = $scope.questionText;
+        $scope.questionData.type = taggingService.getSelectedType().name;
+        //Grab Tags Here
+        $scope.questionData.difficulty = parseInt($("#diff-input").val());
+        $scope.questionData.answers = [];
+        $('.answer-box').each(function(index) {
+           $scope.questionData.answers.push($(this).val()); 
+        });
+        
+        //Check if we are edit or create
+        $scope.createQuestion();
+    }
+    
+    
+    //Interaction with question database
+    $scope.createQuestion = function () {
+        $http.post('/question',  $scope.questionData).success(function(created){
+                $window.location.href = './#qm';
+            });
+    }
+    
+    $scope.editQuestion = function() {
+        $http.put('/question', $scope.questionData).success(function(created) {
+           $window.location.href = './#qm'; 
+        });
+    }
     
     $scope.loadQuestion = function() {
         var loc = "" + $window.location;
@@ -56,73 +104,5 @@ function create_question_controller ($scope,$location,$http,$window, taggingServ
         }
 
     }
-    
-    $scope.updateSelectedType = function(value) {
-        taggingService.updateSelectedType(value);
-        $("#subtopic-box").css({"visibility": "hidden"});
-        $scope.current_subtopics = taggingService.getCurrentSubTopics();
-    }
-
-    $scope.createQuestion = function () {
-        $http.post('/question',  $scope.questionData).success(function(created){
-                $window.location.href = './#qm';
-            });
-    }
-    
-    $scope.editQuestion = function() {
-        $http.put('/question', $scope.questionData).success(function(created) {
-           $window.location.href = './#qm'; 
-        });
-    }
-    
-    $scope.addAnswer = function() {
-        $scope.answers.push($scope.answers.length + 1);
-    }
-    
-    $scope.removeAnswer = function() {
-        if ($scope.answers.length > 1) {
-            $scope.answers.pop();
-        }
-    }
-
-    $scope.$on("topicNotification", function(event, args) {
-        $scope.current_subtopics = taggingService.getCurrentSubTopics();
-    });
-    
-    $scope.showPrompt = function($event) {
-        popupService.init('New Subtopic', 'Enter the new subtopic', '', '');
-        popupService.showPrompt($event, $scope.getPrompt);
-    }
-    
-    $scope.getPrompt = function() {
-        $scope.newSub = popupService.getResult();
-        
-        if ($scope.newSub != null) {
-            taggingService.createNewSubTopic($scope.newSub);
-            $scope.current_subtopics = taggingService.getCurrentSubTopics();
-        }
-    }
-    
-    $scope.compileData = function () {
-    
-        $scope.questionData.text = $scope.questionText;
-        $scope.questionData.type = taggingService.getSelectedType().name;
-        $scope.questionData.topic = taggingService.getSelectedTopic().name;
-        $scope.questionData.subtopics = [];
-        $('.checkbox').each(function(index) {
-            if($(this).find('input').is(':checked')) {
-                $scope.questionData.subtopics.push($(this).find('input').data("name"));
-            }
-        });
-        $scope.questionData.difficulty = parseInt($("#diff-input").val());
-        $scope.questionData.answers = [];
-        $('.answer-box').each(function(index) {
-           $scope.questionData.answers.push($(this).val()); 
-        });
-        
-        //Check if we are edit or create
-        $scope.createQuestion();
-    }
-    
 }
 
