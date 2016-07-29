97119887cec11370775b8aa20164c8a714d04222
Hunter Heidenreich <hunter.scott.heidenreich@gmail.com>
2016-07-29 13:04:48 -0400

    Reduce list interviewers controller

--- static/www/control/conduct_interview_list_controller.js
+++ static/www/control/conduct_interview_list_controller.js
@@ -1,5 +1,4 @@
 function conduct_interview_list_controller($scope, $http, socket) {
-
     $scope.sortType     = 'id';
     $scope.sortReverse  = false;
     $scope.searchInterview  = '';
@@ -26,24 +25,6 @@ function conduct_interview_list_controller($scope, $http, socket) {
             });
          });
     }
-    
-    $scope.addInterviewer = function(id) {
-        console.log('Add Interviewer');
-        socket.emit('add-interviewer', 'New');
-        window.location.href = "#conaction/" + id;
-    }
-    
-    socket.on('news', function(data) {
-        $scope.$apply(function () {
-         console.log(data);
-        });
-      });
-    
-    socket.on('notification', function(data) {
-       $scope.$apply(function() {
-        console.log(data);
-       });
-    });
     
     $scope.loadScreen();
 }
