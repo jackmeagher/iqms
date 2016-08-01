c420b5bd75a7803ac2fdcae8609043b8698b58f7
Hunter Heidenreich <hunter.scott.heidenreich@gmail.com>
2016-08-01 12:09:05 -0400

    Create basic chart displays

--- static/www/control/interpret_interview_controller.js
+++ static/www/control/interpret_interview_controller.js
@@ -1,42 +1,95 @@
 function interpret_interview_controller($scope, $http) {
-    $scope.results = ['Overall', 30, 5, 2, ''];
-    $scope.resultsByTag = {
-        'Java': ['Java', 28, 3, 1, '']
+    
+    $scope.overallResultsChart = {};
+    $scope.overallResultsChart.type = "PieChart";
+    $scope.overallResultsChart.data = [
+        ['Score', 'amount'],
+        ['Good', 39],
+        ['Poor', 5],
+        ['Skipped', 3]
+    ];
+    $scope.overallResultsChart.options = {
+        displayExactValues: true,
+        width: 800,
+        height: 600,
+        is3D: true,
+        chartArea: {left:10,top:10,bottom:0,height:"100%"},
+        colors: ['#5cb85c', '#d9534f', '#f5f5f5']
     };
     
-    $scope.resultsChart = {};
-    
-    $scope.resultsChart.type = "ColumnChart";
-    
-    $scope.onions = [
-        {v: "Onions"},
-        {v: 3},
-    ];
-
-    $scope.resultsChart.data = {"cols": [
-        {id: "t", label: "Topping", type: "string"},
-        {id: "s", label: "Slices", type: "number"}
+    $scope.tagResultsChart = {};
+    $scope.tagResultsChart.type = "BarChart";
+    $scope.tagResultsChart.data = {"cols": [
+        {id: "t", label: "Tag", type: "string"},
+        {id: "r", label: "Good", type: "number"},
+        {id: "w", label: "Poor", type: "number"},
+        {id: "s", label: "Skipped", type: "number"}
     ], "rows": [
         {c: [
-            {v: "Mushrooms"},
-            {v: 3},
-        ]},
-        {c: $scope.onions},
-        {c: [
-            {v: "Olives"},
-            {v: 31}
+            {v: "Java"},
+            {v: 15},
+            {v: 4},
+            {v: 1}
         ]},
         {c: [
-            {v: "Zucchini"},
-            {v: 1},
-        ]},
-        {c: [
-            {v: "Pepperoni"},
+            {v: "OOP"},
+            {v: 8},
             {v: 2},
+            {v: 0}
+        ]},
+        {c: [
+            {v: "Javascript"},
+            {v: 24},
+            {v: 1},
+            {v: 2}
         ]}
     ]};
-
-    $scope.resultsChart.options = {
-        'title': 'How Much Pizza I Ate Last Night'
+    $scope.tagResultsChart.options = {
+        isStacked: 'percent',
+        height: 600,
+        legend: {position: 'top', maxLines: 3},
+        hAxis: {
+          minValue: 0,
+          ticks: [0, .3, .6, .9, 1]
+        },
+        colors: ['#5cb85c', '#d9534f', '#f5f5f5']
+    };
+    
+    $scope.diffResultsChart = {};
+    $scope.diffResultsChart.type = "BarChart";
+    $scope.diffResultsChart.data = {"cols": [
+        {id: "t", label: "Difficulty", type: "string"},
+        {id: "r", label: "Good", type: "number"},
+        {id: "w", label: "Poor", type: "number"},
+        {id: "s", label: "Skipped", type: "number"}
+    ], "rows": [
+        {c: [
+            {v: "Junior"},
+            {v: 25},
+            {v: 1},
+            {v: 1}
+        ]},
+        {c: [
+            {v: "Mid"},
+            {v: 8},
+            {v: 2},
+            {v: 0}
+        ]},
+        {c: [
+            {v: "Senior"},
+            {v: 4},
+            {v: 9},
+            {v: 2}
+        ]}
+    ]};
+    $scope.diffResultsChart.options = {
+        isStacked: 'percent',
+        height: 600,
+        legend: {position: 'top', maxLines: 3},
+        hAxis: {
+          minValue: 0,
+          ticks: [0, .3, .6, .9, 1]
+        },
+        colors: ['#5cb85c', '#d9534f', '#f5f5f5']
     };
 }
