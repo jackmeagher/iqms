60baed0dbbb5177637b48755aa1d43a0c6eebd95
Hunter Heidenreich <hunter.scott.heidenreich@gmail.com>
2016-08-10 13:43:43 -0400

    Secure /candidatePosition

--- routes/candidatePosition.js
+++ routes/candidatePosition.js
@@ -4,41 +4,63 @@ var exports = module.exports = {};
 
 exports = module.exports = new Resource('candidatePosition', '/candidatePosition', {
         get: (req, res) => {
-                models.candidatePosition.findAll().then(function (cp) {
+            if(req.query.idToken) {
+                firebase.auth().verifyIdToken(req.query.idToken).then(function(decodedToken) {
+                    var uid = decodedToken.sub;
+                    models.candidatePosition.findAll().then(function (cp) {
                         res.status(200).json({
                             cp: cp
                         });
                     })
+                }).catch(function(error) {
+                    res.status(511).json({error: "Error"});
+                });
+            } else {
+                res.status(401).json({error: "Forbidden"});
+            }
         }
     }, [new Resource('get with ids', '/:id/position/:posId', {
-        //get question by id
         get: (req, res) => {
-            models.candidatePosition.find({
-               where: {
-                candidateId: req.params.id,
-                positionId: req.params.posId
-               }
-             })
-                .then(function (candidatePosition) {
-                    res.status(200).json({
-                        candidatePosition: candidatePosition
-                    });
-                })
+            if(req.query.idToken) {
+                firebase.auth().verifyIdToken(req.query.idToken).then(function(decodedToken) {
+                    var uid = decodedToken.sub;
+                    models.candidatePosition.find({
+                        where: {
+                            candidateId: req.params.id,
+                            positionId: req.params.posId
+                        }
+                    }).then(function (candidatePosition) {
+                        res.status(200).json({
+                                candidatePosition: candidatePosition
+                        });
+                    })
+                }).catch(function(error) {
+                    res.status(511).json({error: "Error"});
+                });
+            } else {
+                res.status(401).json({error: "Forbidden"});
+            }
         }
-    }
-    ),
-        new Resource('get with one id', '/:id', {
-                get: (req, res) => {
-                        models.candidatePosition.find({
-                                where: {
-                                        c_id: req.params.id
-                                }
-                        }).then(function(result) {
-                                res.status(200).json({
-                                   result: result     
-                                });
-                        })
-                }
-        })
-]
-);
+    }), new Resource('get with one id', '/:id', {
+        get: (req, res) => {
+            if(req.query.idToken) {
+                firebase.auth().verifyIdToken(req.query.idToken).then(function(decodedToken) {
+                    var uid = decodedToken.sub;
+                    models.candidatePosition.find({
+                        where: {
+                            c_id: req.params.id
+                        }
+                    }).then(function(result) {
+                        res.status(200).json({
+                            result: result
+                        });
+                    })
+                }).catch(function(error) {
+                    res.status(511).json({error: "Error"});
+                });
+            } else {
+                res.status(401).json({error: "Forbidden"});
+            }
+        }
+    })
+]);
