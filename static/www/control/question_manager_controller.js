e5ac00c76b8d272aa9d88f55dc570ae92abe1fcd
Hunter Heidenreich <hunter.scott.heidenreich@gmail.com>
2016-08-11 14:31:38 -0400

    Reduce question manager controller

--- static/www/control/question_manager_controller.js
+++ static/www/control/question_manager_controller.js
@@ -1,68 +1,64 @@
-function question_manager_controller($scope, $http, taggingService, authService) {
+function question_manager_controller($scope, $http, $location, taggingService, authService) {
     $scope.sortType     = 'id'; // set the default sort type
     $scope.sortReverse  = false;  // set the default sort order
     $scope.searchQuestion   = '';     // set the default search/filter term
 
-    authService.getUserToken(function(idToken) {
-        $http.get('/question?idToken=' + idToken).success(function (data) {
-            $scope._question = data.questions;
-            $scope._question.forEach(function(q, index) {
-                $scope.stringifyTags(q, idToken);
-            });
-            var tags = {};
-
-            $http.get('/tag?idToken=' + idToken).success(function (data) {
-                data.tags.forEach(function(tag, index) {
-                    tags[tag.name] = tag;
-                });
-
-                if (!tags['Skills']) {
-                    taggingService.createNewTag('Skills');
-                }
-
-                if(!tags['Intro']) {
-                    taggingService.createNewTag('Intro');
-                }
-
-                if(!tags['Close']) {
-                    taggingService.createNewTag('Close');
-                }
-
-                if(!tags['Inline']) {
-                    taggingService.createNewTag('Inline');
-                }
-            });
-        });
-    });
+    var mainTags = ['Intro', 'Skills', 'Close', 'Inline'];
 
     $scope.loadQuestion = function(index) {
-        window.location.href = "#ce#" + index;
-    }
+        $location.path('/ce')
+                 .hash(index);
+    };
 
-    $scope.DeleteQuestion = function (index, question) {
+    $scope.deleteQuestion = function (index, question) {
         authService.getUserToken(function(idToken) {
-            $http.delete('/question/' + question.id + '/tags/?idToken=' + idToken).success(function() {
-            });
+            $http.delete('/question/' + question.id + '/tags/?idToken=' + idToken).success(function(){});
             $http.delete('/question/' + question.id + "?idToken=" + idToken).success(function() {
-                $http.get('/question?idToken=' + idToken).success(function(data){
-                    $scope._question = data.questions;
-                    $scope._question.forEach(function(q, index) {
-                        $scope.stringifyTags(q, idToken);
-                    });
-                })
+                loadAllQuestions(idToken);
             });
         });
     };
 
-    
-    $scope.stringifyTags = function(question, idToken) {
+    var loadAllQuestions = function(idToken) {
+        $http.get('/question?idToken=' + idToken).success(function(data){
+            $scope._question = data.questions;
+            $scope._question.forEach(function(q) {
+                stringifyTags(q, idToken);
+            });
+        });
+    };
+
+    var stringifyTags = function(question, idToken) {
         question.taglist = "";
         $http.get('/question/' + question.id + '/tags/?idToken=' + idToken).success(function(data) {
-           data.tags.forEach(function(tag, index) {
+           data.tags.forEach(function(tag) {
                 question.taglist += tag.name + ", ";
            });
            question.taglist = question.taglist.substring(0, question.taglist.length - 2);
         });
-    }
+    };
+
+    var loadAllTags = function(idToken) {
+        var tags = {};
+        $http.get('/tag?idToken=' + idToken).success(function (data) {
+            data.tags.forEach(function(tag, index) {
+                tags[tag.name] = tag;
+            });
+            addMissingTags(tags);
+        });
+    };
+
+    var addMissingTags = function(tags) {
+        mainTags.forEach(function(tag) {
+           if(!tags[tag]) {
+               taggingService.createNewTag(tag);
+           }
+        });
+    };
+
+    authService.getUserToken(function(idToken) {
+        loadAllQuestions(idToken);
+        loadAllTags(idToken);
+    });
 };
 
