152b5049e465951f18c70c0a50111583dcae89f3
Hunter Heidenreich <hunter.scott.heidenreich@gmail.com>
2016-08-12 15:52:13 -0400

    Reduce login controller

--- static/www/control/login_controller.js
+++ static/www/control/login_controller.js
@@ -1,10 +1,3 @@
-/**
- * Created by nick on 4/12/16.
- */
-/**
- * Created by nick on 3/31/16.
- */
-
 function login_controller($scope, $http, userService, authService, $location) {
     $scope.create = false;
     $scope.emailReset = 0;
@@ -15,46 +8,31 @@ function login_controller($scope, $http, userService, authService, $location) {
     };
     
     $scope.loginUser = function() {
-        firebase.auth().signInWithEmailAndPassword($scope.user.email, $scope.user.password)
-            .catch(function(error) {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                // ...
-            })
-            .then(function() {
-                $location.path("/");
-            });
-    }
+        firebase.auth().signInWithEmailAndPassword($scope.user.email, $scope.user.password).then(function() {
+            $location.path("/");
+        });
+    };
     
     $scope.createUser = function() {
-        firebase.auth().createUserWithEmailAndPassword($scope.user.email, $scope.user.password)
-            .catch(function(error) {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                // ...
-            })
-            .then(function() {
-                var user = {
-                    name: $scope.user.email,
-                    role: "Interviewer"
-                };
-                userService.setUserRole(user.role);
-                authService.getUserToken(function(idToken) {
-                    $http.post('/user/?idToken=' + idToken, user).success(function(data) {
-                        console.log(data);
-                        $location.path("/");
-                    });
+        firebase.auth().createUserWithEmailAndPassword($scope.user.email, $scope.user.password).then(function() {
+            var user = {
+                name: $scope.user.email,
+                role: "Interviewer"
+            };
+            userService.setUserRole(user.role);
+            authService.getUserToken(function(idToken) {
+                $http.post('/user/?idToken=' + idToken, user).success(function(data) {
+                    $location.path("/");
                 });
             });
-    }
+        });
+    };
     
     $scope.resetPassword = function() {
         firebase.auth().sendPasswordResetEmail($scope.user.email).then(function() {
             $scope.emailReset = 2;
-          }, function(error) {
-            // An error happened.
-          });
+        }, function(error) {
+            console.error(error);
+        });
     }
 }
